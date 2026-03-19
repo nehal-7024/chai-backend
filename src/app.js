c6ccafa3ffe4bee46dc04e30bd0fb7d1b20376f0
Nehal_Jain <nehalg2612@gmail.com>
2026-03-19 20:35:54 +0530

    add user and video model

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));  //extended: true allows us to parse nested objects in the request body
+app.use(express.static("public"));  //serves static files from the public directory, you can put your images, css files, etc. in the public directory and they will be accessible from the root URL
+app.use(cookieParser());  
+
+
+export { app };
